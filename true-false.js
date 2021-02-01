71b49a588f301496ce2f66406a33b3d3698ae599
FaysalShuvo <shuvonma57@gmail.com>
2021-02-01 22:51:50 +0600

    True vs False

---
+++ true-false.js
@@ -0,0 +1,17 @@
+// const age = -1;
+
+const justAvar = 1;
+
+if (justAvar) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+
+// true
+// "0" , ' ' , []
+// False
+// undefined
+// null
+// NaN
+// ""
