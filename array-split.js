275cba183ba97304f55c3458dc15ede8bdf52229
FaysalShuvo <shuvonma57@gmail.com>
2021-02-02 12:43:01 +0600

    array-split

---
+++ array-split.js
@@ -0,0 +1,10 @@
+const numbs = [1, 2, 3, 4, 5, 6, 7, 8];
+const part = numbs.slice(2, 5);
+// console.log(part);
+// console.log(numbs);
+// const removed = numbs.splice(2, 5, 33, 44, 55, 666);
+// console.log(removed);
+// console.log(numbs);
+
+const together = numbs.join("  ");
+console.log(together);
---
+++ scope.js
@@ -0,0 +1,13 @@
+let bonus = 20;
+
+function sum(first, second) {
+    let result = first + second +bonus;
+    return result;  
+    if (result > 9) {
+        const mood = "happy"
+        console.log(mood);
+    }
+}
+
+const output = sum(7, 3); 
+console.log(output);
